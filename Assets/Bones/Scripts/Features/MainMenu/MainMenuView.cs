5da012259598e21724c8ce3d7521fa5ed460698b
alvmivan <alvmivan@gmail.com>
2021-01-30 09:06:37 -0300

    menu

--- Assets/Bones/Scripts/Features/MainMenu/MainMenuView.cs
+++ Assets/Bones/Scripts/Features/MainMenu/MainMenuView.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using Bones.Scripts.Architecture.ViewManager;
 using Bones.Scripts.Features.Audio.Music;
+using Bones.Scripts.Features.Currency.Services;
 using Bones.Scripts.Features.Settings.View;
 using CurrentGame;
 using GameName;
+using Gameplay;
 using Injector.Core;
 using UniRx;
-
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,10 +17,12 @@ namespace Bones.Scripts.Features.MainMenu
     {
         [SerializeField] private Button settingsButton;
         [SerializeField] private Button exitButton;
-        [SerializeField] private Button startGame;
+        [SerializeField] private Button newGame;
+        [SerializeField] private Button continueGame;
 
         private readonly CompositeDisposable disposables = new CompositeDisposable();
         private ViewManager viewManager;
+        private CurrenciesService currencies;
 
         protected override void OnInit()
         {
@@ -28,14 +31,15 @@ namespace Bones.Scripts.Features.MainMenu
 
         protected override void OnShow()
         {
+            currencies = Injection.Get<CurrenciesService>();
             Injection.Get<MusicPlayer>().Play("menu");
-            
+
             settingsButton
                 .OnClickAsObservable()
                 .SelectMany(_ => viewManager.GetOutSwipe("settings"))
                 .Subscribe()
                 .AddTo(disposables);
-            
+
             exitButton
                 .OnClickAsObservable()
                 .Do(_ =>
@@ -48,11 +52,28 @@ namespace Bones.Scripts.Features.MainMenu
                 })
                 .Subscribe()
                 .AddTo(disposables);
-            startGame
+
+
+            newGame
                 .OnClickAsObservable()
+                .Do(_ => currencies[GlobalProperties.LevelsCurrencyName] = 0)
                 .Select(_ => viewManager.GetOut("game"))
                 .Subscribe()
                 .AddTo(disposables);
+
+            if (currencies[GlobalProperties.LevelsCurrencyName] > 0)
+            {
+                continueGame.gameObject.SetActive(true);
+                continueGame
+                    .OnClickAsObservable()
+                    .Select(_ => viewManager.GetOut("game"))
+                    .Subscribe()
+                    .AddTo(disposables);
+            }
+            else
+            {
+                continueGame.gameObject.SetActive(false);
+            }
         }
 
 
